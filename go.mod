bd236943df35d465295180eef142b20225b84891
Gregor Best <gbe@unobtanium.de>
2020-04-08 20:52:29 +0200

    First step of ACME integration

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/farhaven/Acme-Git
 go 1.13
 
 require (
+	9fans.net/go v0.0.2
 	github.com/go-git/go-git v4.7.0+incompatible
 	github.com/go-git/go-git/v5 v5.0.0
 	golang.org/x/crypto v0.0.0-20200406173513-056763e48d71 // indirect
--- main.go
+++ main.go
@@ -2,30 +2,63 @@ package main
 
 import (
 	"log"
+	"os"
 
+	"9fans.net/go/acme"
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
 )
 
+// winFatal writes a formatted message to win's Error window and calls log.Fatalf
+func winFatal(win *acme.Win, fmt string, args ...interface{}) {
+	win.Errf(fmt, args...)
+	log.Fatalf(fmt, args...)
+}
+
 func main() {
-	log.Println("here we go")
+	win, err := acme.New()
+	if err != nil {
+		log.Fatalln("can't open ACME window: %s", err)
+	}
+
+	wd, err := os.Getwd()
+	if err != nil {
+		winFatal(win, "can't get working directory: %s", err)
+	}
+
+	win.Name("%s/-git", wd)
 
 	repo, err := git.PlainOpen(".")
 	if err != nil {
-		log.Fatalln("can't open repo:", err)
+		winFatal(win, "can't open repo: %s", err)
 	}
 
-	log.Println("got repo", repo)
+	win.Fprintf("data", "got repo: %v\n\n", repo)
 
+	// Working tree status
+	tree, err := repo.Worktree()
+	if err != nil {
+		winFatal(win, "can't get work tree: %s", err)
+	}
+	status, err := tree.Status()
+	if err != nil {
+		winFatal(win, "can't get status: %s", err)
+	}
+
+	win.Fprintf("data", "Tree status:\n%s\n", status)
+
+	// List branches
 	branches, err := repo.Branches()
 	if err != nil {
-		log.Fatalln("can't get branches:", err)
+		winFatal(win, "can't get branches: %s", err)
 	}
 	defer branches.Close()
 
-	log.Println("branches:")
+	win.Fprintf("data", "Branches:\n")
 	branches.ForEach(func(ref *plumbing.Reference) error {
-		log.Println(ref.Name())
+		win.Fprintf("data", "\t%s\n", ref.Name())
 		return nil
 	})
+
+	win.Ctl("clean")
 }
